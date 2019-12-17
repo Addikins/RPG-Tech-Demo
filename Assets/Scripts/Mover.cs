e8531bff5d08c7a7ff15e03b01d79b7a8da31581
Addikins <addikinsplays@gmail.com>
2019-12-16 22:21:33 -0700

    Add Animations and blend tree
    Set animation to change speed relative to player speed from Nav Mesh Agent in Mover script

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,16 +6,17 @@ using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] Transform target;
-
     Ray lastRay;
-    // Update is called once per frame
+
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
                 MoveToCursor();
         }
+        UpdateAnimator();
+
     }
 
     private void MoveToCursor()
@@ -27,5 +28,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 loclalVelocity = transform.InverseTransformDirection(velocity);
+        float speed = loclalVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
