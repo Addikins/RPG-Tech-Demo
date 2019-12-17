a732f5505ddd6d1e5edc5a20634397bad08caf63
Addikins <addikinsplays@gmail.com>
2019-12-17 02:05:26 -0700

    Add multiple scripts
    Player now recognizes combat targets and stops within weapon range of target
    Player can move to and away from target without error
    Setup ActionScheduler.cs

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace RPG.Core {
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
+}
 }
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        NavMeshAgent navMeshAgent;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 loclalVelocity = transform.InverseTransformDirection(velocity);
+            float speed = loclalVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+    }
+}
