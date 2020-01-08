46e0993d618bb67e06708706fec7b9acc5669aeb
Addikins <addikinsplays@gmail.com>
2020-01-08 09:59:58 -0700

    Edit music player and scene 0

--- Assets/Scripts/Audio/AudioRandomizer.cs
+++ Assets/Scripts/Audio/AudioRandomizer.cs
@@ -6,12 +6,13 @@ public class AudioRandomizer : MonoBehaviour
 {
     [SerializeField] AudioClip[] audioClips = null;
     [SerializeField] GameObject parentObject = null;
+    [SerializeField] float volume = .5f;
 
     int randomClip;
 
     public void PlayRandomClip()
     {
         randomClip = Random.Range(0, audioClips.Length);
-        AudioSource.PlayClipAtPoint(audioClips[randomClip], parentObject.transform.position);
+        AudioSource.PlayClipAtPoint(audioClips[randomClip], parentObject.transform.position, volume);
     }
 }
